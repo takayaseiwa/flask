3680a37b72e53521e755523d58e87c4c1022d573
Takaya Seiwa <ljks1021@gmail.com>
2020-06-10 20:23:01 +0900

    sqlite使った

--- app.py
+++ app.py
@@ -19,6 +19,30 @@ def greet(text):
 def notfound(code):
     return "404ペーーーーーーージ"
 
+
+#データベースの接続
+@app.route("/dbtest")
+def dbtest():
+    #データベースに接続
+    conn = sqlite3.connect('flask.db')
+    #どこのデータを抜くかカーソルを充てる。カーソル→目印
+    c = conn.cursor()
+    #ecebute:実行する
+    c.execute("select name, adress from users where id = 1")
+    # fitchone:フェッチ：実査に取得する
+    user_info = c.fetchone(）
+    # データベース接続終了
+    c.close()
+
+    
+    print(user_info)
+    return render_template("dbtest.html", user_info = user_info)
+
+
+
+
+
+#-------------これより下にすると表示されない----------------
 if __name__ == "__main__":
     #サーバーを起動するよ
     app.run(debug=True)
